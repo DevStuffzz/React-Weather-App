e8847cce74a19fdc6feeca9782881ccb065d28d0
Corey Beaver <coreybeaver@Coreys-MacBook-Pro.local>
2025-10-15 03:45:23 -0400

    updated base

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// Replace the base with your GitHub Pages repo name
 export default defineConfig({
   plugins: [react()],
-  
-})
+  base: "/react.weather.app/"
+});
